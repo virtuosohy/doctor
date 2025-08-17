fea54fb9e2153bcddc8ba04cfb6994c0e1b96daf
virtuosohy <virtuosohy@outlook.com>
2025-08-17 18:07:47 +0800

    8.16

--- pzadmin/src/views/login/index.vue
+++ pzadmin/src/views/login/index.vue
@@ -1,11 +1,148 @@
 <script setup>
+import { ref,reactive} from 'vue'
+import {getCode} from "../../api/index"
+import {UserFilled ,Lock} from "@element-plus/icons-vue";
+import {ElMessage} from "element-plus";
+const imgUrl = new URL("../../../public/login-head.png",import.meta.url).href
+//切换表单（0表示登录，1表示注册）
+const formType = ref(0)
+const handleChange = () =>{
+  formType.value = formType.value ? 0 : 1
+}
 
+//表单初识值
+const loginForm = reactive({
+  userName: "",
+  passWord:"",
+  validCode:""
+})
+
+//发送短信倒计时
+const countdown = reactive({
+  validText: "获取验证码",
+  time: 60,
+})
+
+//账号校验
+const validateUser = (rule,value,callback)=> {
+   //账号不为空
+   if(value === ''){
+     return callback(new Error('请输入账号'))
+   }else{
+     const phoneReg = /^1(3[0-9]|4[01456879]|5[0-35-9]|6[2567]|7[0-8]|8[0-9]|9[0-35-9])\d{8}$/
+     phoneReg.test(value) ? callback() : callback(new Error('请输入正确的手机号'))
+   }
+}
+
+//密码校验
+const validatepass = (rule,value,callback)=> {
+  //密码不为空
+  if(value === ''){
+    return callback(new Error('请输入密码'))
+  }else{
+    const passReg = /^[a-zA-Z0-9]{6,16}$/
+    passReg.test(value) ? callback() : callback(new Error('请输入6-16位数字或字母'))
+  }
+}
+
+//表单验证
+const rules = reactive({
+  userName: [{
+    validator:validateUser,
+    trigger: 'blur'
+  }],
+  passWord:[{
+    validator:validatepass,
+    trigger: 'blur'
+  }]
+})
+let  disabled = false
+const countdownChange = () =>{
+  if(disabled) return
+  //判断手机号是否真确
+  const phoneReg = /^1(3[0-9]|4[01456879]|5[0-35-9]|6[2567]|7[0-8]|8[0-9]|9[0-35-9])\d{8}$/
+  if(!loginForm.userName ||!phoneReg.test(loginForm.userName) ){
+    return ElMessage({
+      message: "请输入正确的手机号",
+      type: "warning",
+    })
+  }
+  setInterval(() =>{
+    if(countdown.time <= 0){
+      countdown.validText = "获取验证码"
+      countdown.time = 60
+      disabled = false
+    }else{
+      countdown.time--
+      countdown.validText = "剩余" + countdown.time + "s"
+    }
+  },1000)
+  disabled = true
+  getCode({tel:loginForm.userName}).then(({data}) => {
+     if(data.code === 10000){
+       ElMessage.success("发送成功")
+     }
+  })
+}
+
+//提交表单
+const submit = () => {
+
+}
 </script>
 
 <template>
-<div>登录</div>
+  <el-row class="login-container" justify="center" :align="'middle'">
+    <el-card style="max-width: 480px">
+      <template #header>
+        <div class="card-header">
+          <img :src="imgUrl" alt="" >
+        </div>
+      </template>
+      <div class="jump-link">
+        <el-link type="primary" @click="handleChange" >{{formType ? '注册账号' :'登录'}}</el-link>
+      </div>
+      <el-form
+          :model="loginForm"
+          style="max-width: 600px"
+          class="demo-ruleForm"
+          :rules="rules">
+        <el-form-item prop="userName" >
+          <el-input v-model="loginForm.userName" placeholder="手机号" :prefix-icon="UserFilled"></el-input>
+        </el-form-item>
+        <el-form-item prop="passWord" >
+          <el-input v-model="loginForm.passWord" type="password" placeholder="密码" :prefix-icon="Lock" ></el-input>
+        </el-form-item>
+        <el-form-item prop="validCode" v-if="formType">
+          <el-input v-model="loginForm.validCode"  placeholder="验证码" :prefix-icon="Lock" >
+            <template #append>
+              <span @click="countdownChange">{{ countdown.validText }}</span>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width: 100%" @click="submit">{{formType ? '注册' : '登录'}}</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </el-row>
 </template>
 
-<style scoped>
-
+<style lang="less" scoped>
+:deep(.el-card__header) {
+  padding: 0
+}
+.login-container {
+  height: 100%;
+  .card-header{
+    background-color: #899fe1;
+    img {
+      width: 430px;
+    }
+  }
+  .jump-link {
+    text-align: right;
+    margin-bottom: 10px;
+  }
+}
 </style>
