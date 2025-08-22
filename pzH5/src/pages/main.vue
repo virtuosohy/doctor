f050fe9652a1a8e22df392a110e77ff62fe9e100
virtuosohy <virtuosohy@outlook.com>
2025-08-22 19:48:20 +0800

    8.22

---
+++ pzH5/src/pages/main.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>11111</div>
+ <RouterView />
+</template>
+
+<style scoped lang="less">
+
+</style>
---
+++ pzH5/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from '@vant/auto-import-resolver';
+
+export default {
+  server:{
+    port: 3000,
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [VantResolver()],
+    }),
+    Components({
+      resolvers: [VantResolver()],
+    }),
+  ],
+};
