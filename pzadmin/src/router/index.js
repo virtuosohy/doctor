b70a7345a11a508d8a94f8609fa14984b42b0d8e
virtuosohy <virtuosohy@outlook.com>
2025-08-28 18:42:40 +0800

    8.28

--- pzadmin/src/router/index.js
+++ pzadmin/src/router/index.js
@@ -1,11 +1,5 @@
 import Layout from '../views/Main.vue'
 import Login from '../views/login/index.vue'
-import Admin from '../views/auth/admin/index.vue'
-import Group from '../views/auth/group/index.vue'
-import Order from '../views/vppz/order/index.vue'
-import Staff from '../views/vppz/staff/index.vue'
-import Dashboard from '../views/dashboard/index.vue'
-
 import { createRouter, createWebHashHistory } from "vue-router";
 
 
